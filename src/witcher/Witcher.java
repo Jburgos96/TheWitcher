2ff790869d07a0b0281b19d7a7647b0375a9f5a7
JorgeBurgos <jorge.burgos.barrera@gmail.com>
2023-01-11 12:47:54 +0100

    Añadidos getters y setters

--- src/witcher/Witcher.java
+++ src/witcher/Witcher.java
@@ -2,6 +2,30 @@ package witcher;
 
 public class Witcher {
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public boolean isHasSoul() {
+		return hasSoul;
+	}
+
+	public void setHasSoul(boolean hasSoul) {
+		this.hasSoul = hasSoul;
+	}
+
 	private String name;
 	private int health;
 	private boolean hasSoul;
